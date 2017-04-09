c5e932c4ffe56c9a7b29a25adba09b03d2c2d2b3
Jose Corbacho <JoseAntonio.Corbacho@sky.uk>
2017-04-09 11:49:34 +0100

    P2 #2 Simplify image layers to one

--- Term1/CarND-Traffic-Sign-Classifier-Project/Traffic_Sign_Classifier.py
+++ Term1/CarND-Traffic-Sign-Classifier-Project/Traffic_Sign_Classifier.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # # Self-Driving Car Engineer Nanodegree
@@ -33,7 +32,7 @@ import matplotlib.pyplot as plt
 # TODO: Fill this in based on where you saved the training and testing data
 data_dir = './traffic-signs-data/'
 training_file = os.path.join(data_dir, 'train.p')
-validation_file= os.path.join(data_dir, 'valid.p')
+validation_file = os.path.join(data_dir, 'valid.p')
 testing_file = os.path.join(data_dir, 'test.p')
 
 with open(training_file, mode='rb') as f:
@@ -42,11 +41,10 @@ with open(validation_file, mode='rb') as f:
     valid = pickle.load(f)
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
-    
+
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
-
 
 # ---
 # 
@@ -89,7 +87,6 @@ print("Number of testing examples =", n_test)
 print("Image data shape =", image_shape)
 print("Number of classes =", n_classes)
 
-
 # ### Include an exploratory visualization of the dataset
 
 # Visualize the German Traffic Signs Dataset using the pickled file(s). This is open ended, suggestions include: plotting traffic sign images, plotting the count of each sign, etc. 
@@ -105,6 +102,7 @@ print("Number of classes =", n_classes)
 
 import random
 
+
 # Visualizations will be shown in the notebook.
 
 
@@ -112,7 +110,7 @@ def show_random_image(img, cmap=None):
     plt.figure(figsize=(2, 2))
     plt.imshow(img, cmap=cmap)
 
-    
+
 def plot_count_signals(data):
     signals, count = np.unique(data, return_counts=True)
     n_groups = len(count)
@@ -127,10 +125,10 @@ def plot_count_signals(data):
 
     opacity = 0.4
 
-    rects1 = plt.bar(index, count, bar_width,
-                     alpha=opacity,
-                     color='b',
-                     label='Signals')
+    plt.bar(index, count, bar_width,
+            alpha=opacity,
+            color='b',
+            label='Signals')
 
     plt.xlabel('Signal Id')
     plt.ylabel('Count')
@@ -144,8 +142,8 @@ def plot_count_signals(data):
 
 def show_histogram(img):
     hist, bin_edges = np.histogram(img, bins=60)
-    bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
-    plt.figure(figsize=(11,4))
+    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+    plt.figure(figsize=(11, 4))
 
     plt.subplot(132)
     plt.plot(bin_centers, hist, lw=2)
@@ -172,9 +170,9 @@ def visualize_data():
     plot_count_signals(y_train)
     plot_count_signals(y_valid)
     plot_count_signals(y_test)
+    plt.hist(y_train, n_classes)
+    plt.show()
 
-plt.hist(y_train, n_classes)
-plt.show()
 # visualize_data()
 
 
@@ -215,24 +213,23 @@ plt.show()
 
 import cv2
 
+
 def normalize_data(data):
-    return (data -128.) / 128.
+    return (data - 128.) / 128.
+
 
 def convert_gray(data):
     # cvtColor return 32x32
     converted = []
     for row in data:
-        cvt = cv2.cvtColor(row, cv2.COLOR_BGR2GRAY)
-        row[:, :, 0] = cvt
-        row[:, :, 1] = cvt
-        row[:, :, 2] = cvt
-        converted.append(row)
+        converted.append(cv2.cvtColor(row, cv2.COLOR_BGR2GRAY))
     return np.array(converted)
+
 
 def rotate_image(img):
     num_rows, num_cols = img.shape[:2]
 
-    rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2), 10, 1)
+    rotation_matrix = cv2.getRotationMatrix2D((num_cols / 2, num_rows / 2), 10, 1)
     return cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
 
 
@@ -247,10 +244,20 @@ def apply_denoising(img):
     # Apply Gaussian smoothering
     pass
 
+
 X_train = convert_gray(X_train)
+X_train = X_train[Ellipsis, np.newaxis]
+
 X_valid = convert_gray(X_valid)
+X_valid = X_valid[Ellipsis, np.newaxis]
+
 X_train = normalize_data(X_train)
 X_valid = normalize_data(X_valid)
+
+# X_test must be converted and normalized too
+X_test = convert_gray(X_test)
+X_test = X_test[Ellipsis, np.newaxis]
+X_test = normalize_data(X_test)
 
 image, index = get_random_image(X_train)
 print('{}: {}'.format('Index', y_train[index]))
@@ -259,16 +266,17 @@ show_random_image(image, 'gray')
 image = rotate_image(image)
 show_random_image(image)
 
-
 # In[ ]:
 
 # Extend the dataset
-X_train = np.append(X_train, rotate_dataset(X_train), axis=0)
+rotated = rotate_dataset(X_train)
+rotated = rotated[Ellipsis, np.newaxis]
+print('{}/{}'.format(X_train.shape, rotated.shape))
+X_train = np.append(X_train, rotated, axis=0)
 y_train = np.append(y_train, y_train, axis=0)
 
 print(X_train.shape)
 print(X_valid.shape)
-
 
 # ### Model Architecture
 
@@ -279,15 +287,16 @@ print(X_valid.shape)
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 
-def LeNet(x, use_dropout=False):    
+
+def LeNet(x, use_dropout=False):
     # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
     mu = 0
     sigma = 0.1
-    
+
     # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = mu, stddev = sigma))
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
     conv1_b = tf.Variable(tf.zeros(6))
-    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
     # SOLUTION: Activation.
     conv1 = tf.nn.relu(conv1)
@@ -298,10 +307,10 @@ def LeNet(x, use_dropout=False):
     conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
     conv2_b = tf.Variable(tf.zeros(16))
-    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-    
+    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+
     # SOLUTION: Activation.
     conv2 = tf.nn.relu(conv2)
     if use_dropout:
@@ -311,43 +320,42 @@ def LeNet(x, use_dropout=False):
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
-    fc0   = flatten(conv2)
-    
+    fc0 = flatten(conv2)
+
     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
     fc1_b = tf.Variable(tf.zeros(120))
-    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
-    
+    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
+
     # SOLUTION: Activation.
-    fc1    = tf.nn.relu(fc1)
+    fc1 = tf.nn.relu(fc1)
     if use_dropout:
         fc1 = tf.nn.dropout(fc1, keep_prob)
 
     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(84))
-    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
-    
+    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
+
     # SOLUTION: Activation.
-    fc2    = tf.nn.relu(fc2)
+    fc2 = tf.nn.relu(fc2)
     if use_dropout:
         fc2 = tf.nn.dropout(fc2, keep_prob)
 
     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
-    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean = mu, stddev = sigma))
-    fc3_b  = tf.Variable(tf.zeros(n_classes))
+    fc3_W = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean=mu, stddev=sigma))
+    fc3_b = tf.Variable(tf.zeros(n_classes))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
-    
+
     return logits
 
 
 # In[ ]:
 
-x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, n_classes)
 keep_prob = tf.placeholder(tf.float32)
-
 
 # In[ ]:
 
@@ -356,9 +364,8 @@ rate = 0.001
 logits = LeNet(x)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
 loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+optimizer = tf.train.AdamOptimizer(learning_rate=rate)
 training_operation = optimizer.minimize(loss_operation)
-
 
 # In[ ]:
 
@@ -366,13 +373,14 @@ correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 saver = tf.train.Saver()
 
+
 def evaluate(X_data, y_data):
     num_examples = len(X_data)
     total_accuracy = 0
     sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob:1.0})
+        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
 
@@ -385,9 +393,9 @@ def evaluate(X_data, y_data):
 # In[ ]:
 
 from sklearn.utils import shuffle
+
 EPOCHS = 10
 BATCH_SIZE = 128
-
 
 # In[ ]:
 
@@ -399,7 +407,7 @@ BATCH_SIZE = 128
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     num_examples = len(X_train)
-    
+
     print("Training...")
     print()
     for i in range(EPOCHS):
@@ -407,13 +415,13 @@ with tf.Session() as sess:
         for offset in range(0, num_examples, BATCH_SIZE):
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob:0.5})
-            
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5})
+
         validation_accuracy = evaluate(X_valid, y_valid)
-        print("EPOCH {} ...".format(i+1))
+        print("EPOCH {} ...".format(i + 1))
         print("Validation Accuracy = {:.3f}".format(validation_accuracy))
         print()
-    
+
     test_accuracy = evaluate(X_test, y_test)
     print("Test Accuracy = {:.3f}".format(test_accuracy))
     saver.save(sess, './traffic_classifier')
@@ -536,24 +544,24 @@ with tf.Session() as sess:
 # activation_min/max: can be used to view the activation contrast in more detail, by default matplot sets min and max to the actual min and max values of the output
 # plt_num: used to plot out multiple different weight feature map sets on the same block, just extend the plt number for each new feature map entry
 
-def outputFeatureMap(image_input, tf_activation, activation_min=-1, activation_max=-1 ,plt_num=1):
+def outputFeatureMap(image_input, tf_activation, activation_min=-1, activation_max=-1, plt_num=1):
     # Here make sure to preprocess your image_input in a way your network expects
     # with size, normalization, ect if needed
     # image_input =
     # Note: x should be the same name as your network's tensorflow data placeholder variable
     # If you get an error tf_activation is not defined it may be having trouble accessing the variable from inside a function
-    activation = tf_activation.eval(session=sess,feed_dict={x : image_input})
+    activation = tf_activation.eval(session=sess, feed_dict={x: image_input})
     featuremaps = activation.shape[3]
-    plt.figure(plt_num, figsize=(15,15))
+    plt.figure(plt_num, figsize=(15, 15))
     for featuremap in range(featuremaps):
-        plt.subplot(6,8, featuremap+1) # sets the number of feature maps to show on each row and column
-        plt.title('FeatureMap ' + str(featuremap)) # displays the feature map number
+        plt.subplot(6, 8, featuremap + 1)  # sets the number of feature maps to show on each row and column
+        plt.title('FeatureMap ' + str(featuremap))  # displays the feature map number
         if activation_min != -1 & activation_max != -1:
-            plt.imshow(activation[0,:,:, featuremap], interpolation="nearest", vmin =activation_min, vmax=activation_max, cmap="gray")
+            plt.imshow(activation[0, :, :, featuremap], interpolation="nearest", vmin=activation_min,
+                       vmax=activation_max, cmap="gray")
         elif activation_max != -1:
-            plt.imshow(activation[0,:,:, featuremap], interpolation="nearest", vmax=activation_max, cmap="gray")
-        elif activation_min !=-1:
-            plt.imshow(activation[0,:,:, featuremap], interpolation="nearest", vmin=activation_min, cmap="gray")
+            plt.imshow(activation[0, :, :, featuremap], interpolation="nearest", vmax=activation_max, cmap="gray")
+        elif activation_min != -1:
+            plt.imshow(activation[0, :, :, featuremap], interpolation="nearest", vmin=activation_min, cmap="gray")
         else:
-            plt.imshow(activation[0,:,:, featuremap], interpolation="nearest", cmap="gray")
-
+            plt.imshow(activation[0, :, :, featuremap], interpolation="nearest", cmap="gray")
